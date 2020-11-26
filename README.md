4d92bd3773c5cd70e3b81fdcfe0ae1ceda799e7f
kelvin012 <k20kelvin20@hotmail.com>
2020-11-26 04:34:36 -0300

    :key: New features

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ### usage:
-##### python run.py crypt [ chave ] [ mensagem que sera criptografada ]
-##### python run.py decrypt [ chave ] [ mensagem que sera descriptografada ]
-##### python run.py crypt | decrypt [ chave ] [ -p (para pegar texto da clipbord) ]
-##### example:
+##### python run.py crypt [ chave ] [ texto que sera criptografado ]
+##### python run.py decrypt [ chave ] [ texto que sera descriptografado ]
+##### python run.py decrypt [ -brute (Para testar usando a chaves de 1 a 25) ] [ texto ]
+##### python run.py crypt | decrypt [ chave ] [ -p (Para usar o texto da clipboard) ]
+##### examples:
 ###### python run.py crypt 3 this is a test message
 ###### python run.py decrypt 3 wklv lv d whvw phvvdjh
+###### python run.py decrypt brute wklv lv d whvw phvvdjh
 ###### python run.py crypt | decrypt 3 -p
--- run.py
+++ run.py
@@ -2,12 +2,17 @@ import sys
 import pyperclip
 
 alfabeto = []
-alfabeto2x = []
+alfabeto_upper = []
 
 for i in range(ord('a'), ord('z') + 1):
     alfabeto.append(chr(i))
 
 alfabeto2x = alfabeto[:] + alfabeto[:]
+
+for i in range(ord('A'), ord('Z') + 1):
+    alfabeto_upper.append(chr(i))
+
+alfabeto2x_upper = alfabeto_upper[:] + alfabeto_upper[:]
 
 
 def main(args):
@@ -22,12 +27,30 @@ def main(args):
 
     while True:
         if len(args) <= 3 or (args[3] == '-p' and len(args) > 4):
-            print('usage: \npython run.py crypt [ chave ] [ mensagem que sera criptografada ]')
-            print('python run.py decrypt [ chave ] [ mensagem que sera descriptografada ]')
-            print('python run.py crypt | decrypt [ chave ] [ -p (para pegar texto da clipbord) ]')
+            print(
+                '''   _____                            _       _       _               
+  / ____|                          | |     (_)     | |              
+ | |     __ _  ___  ___  __ _ _ __/ __) ___ _ _ __ | |__   ___ _ __ 
+ | |    / _` |/ _ \/ __|/ _` | '__\__ \/ __| | '_ \| '_ \ / _ \ '__|
+ | |___| (_| |  __/\__ \ (_| | |  (   / (__| | |_) | | | |  __/ |   
+  \_____\__,_|\___||___/\__,_|_|   |_| \___|_| .__/|_| |_|\___|_|   
+                                             | |                    
+                                             |_|                    ''')
+            print('Usage: \npython run.py crypt [ chave ] [ texto que sera criptografado ]')
+            print('python run.py decrypt [ chave ] [ texto que sera descriptografado ]')
+            print('python run.py decrypt [ brute (Para testar usando a chaves de 1 a 25) ] [ texto ]')
+            print('python run.py crypt | decrypt [ chave ] [ -p (Para usar o texto da clipboard) ]')
+            break
+        elif args[1] == 'decrypt' and args[2] == 'brute':
+            for k in range(1, 25 + 1):
+                out = decrypt(text_string, k)
+                print(f'[{k}] {out}')
+            break
+        elif args[1] == 'crypt' and args[2] == 'brute':
+            print('Metodo crypt não possui suporte para bruteforce!')
             break
         else:
-            if int(args[2]) >= 1 and int(args[2]) <= 32:
+            if 1 <= int(args[2]) <= 25:
                 if args[3] == '-p' and len(args) <= 4:
                     if args[1] == 'crypt':
                         out = crypt(str(pyperclip.paste()).lower(), int(args[2]))
@@ -49,16 +72,19 @@ def main(args):
                         print(out)
                         break
             else:
-                print('A chave precisa ser um numero entre 1 e 32')
+                print('A chave precisa ser um numero entre 1 e 25')
                 break
 
 
 def crypt(texto, chave):
     mensagem_crypt = ''
     for letter in texto:
-        if letter.isalpha():
+        if letter.isalpha() and letter.islower():
             pos_letra = alfabeto.index(letter)
             mensagem_crypt += alfabeto2x[pos_letra + chave]
+        elif letter.isalpha() and letter.isupper():
+            pos_letra = alfabeto_upper.index(letter)
+            mensagem_crypt += alfabeto2x_upper[pos_letra + chave]
         else:
             mensagem_crypt += letter
     return mensagem_crypt
@@ -68,9 +94,12 @@ def decrypt(texto, chave):
     teste = texto[:]
     mensagem_decrypt = ''
     for letra in teste:
-        if letra.isalpha():
+        if letra.isalpha() and letra.islower():
             pos_letra = alfabeto2x.index(letra)
             mensagem_decrypt += alfabeto2x[pos_letra - chave]
+        elif letra.isalpha() and letra.isupper():
+            pos_letra = alfabeto2x_upper.index(letra)
+            mensagem_decrypt += alfabeto2x_upper[pos_letra - chave]
         else:
             mensagem_decrypt += letra
     return mensagem_decrypt
